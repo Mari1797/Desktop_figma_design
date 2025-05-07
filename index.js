dc0756a403286308d6a472afb465834180f51995
Mari <marianisidahomen@gmail.com>
2025-05-07 21:25:45 +0100

    implemented authenication

--- index.js
+++ index.js
@@ -1,3 +1,83 @@
+// index.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyB17qm8SoOB3Z4q7tbZl5n5Zqh-NaSvoIc",
+  authDomain: "portfolio-70f27.firebaseapp.com",
+  projectId: "portfolio-70f27",
+  storageBucket: "portfolio-70f27.appspot.com",
+  messagingSenderId: "768143289195",
+  appId: "1:768143289195:web:eea8e5c5c9b2a89a2d7a2e",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+auth.languageCode = "en";
+
+document.getElementById("google-btn").addEventListener("click", () => {
+  const mediaQuery = window.matchMedia("(min-width: 769px)");
+
+  if (mediaQuery.matches) {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        window.location.href = "./Login/Login.html";
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  } else {
+    const auth = getAuth();
+    signInWithRedirect(auth, provider);
+    window.location.href = "./Login/Login.html";
+  }
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+});
+
 const form = document.querySelector(".form");
 const user_name = document.querySelector(".name");
 const email = document.querySelector(".email");
